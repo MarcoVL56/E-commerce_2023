dd97c2affa7d2488374e3ab86792b3f09d44a7d0
MarcoVL <marco1111vl@gmail.com>
2023-02-17 18:16:08 -0600

    Se agregaron unos elementos

--- src/App.js
+++ src/App.js
@@ -2,33 +2,19 @@ import React from 'react';
 import './App.css';
 import Encabezado from "./componentes/Encabezado/Encabezado"
 import Navegacion from "./componentes/Navegacion/Navegacion"
-import Principal from "./componentes/Principal/Principal"   
+import Principal from "./componentes/Principal/Principal"
 import Productos from "./componentes/Productos/Productos"
 import PiePagina from "./componentes/PiePagina/PiePagina"
 
 
 function App() {
   return (
-    <div>
-      <header>
-        <Encabezado/>
-      </header>
-
-      <nav>
-        <Navegacion/>
-      </nav>
-
-      <section>
-        <Principal/>
-      </section>
-
-      <section>
-        <Productos/>
-      </section>
-
-      <footer>
-        <PiePagina/>
-      </footer>
+    <div className='contenedor-disposicion'>
+      <Encabezado />
+      <Navegacion />
+      <Principal />
+      <Productos />
+      <PiePagina />
     </div>
   );
 }
--- src/componentes/Encabezado/Encabezado.js
+++ src/componentes/Encabezado/Encabezado.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./Encabezado.css"
 
 function Encabezado(props){
+    return(
+        <header className="encabezado">
+            <div className="contenedor-inicial">
+                <div className="contenedor-inicial--elementos">
+                    
+                </div>
+                <div className="contenedor-inicial--elementos">
 
+                </div>
+                <div className="contenedor-inicial--elementos">
+
+                </div>
+            </div>
+        </header>
+    )
 }
 
 
